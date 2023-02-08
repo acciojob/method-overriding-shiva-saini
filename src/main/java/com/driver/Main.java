5bb8f2fe7b58a3ee78121fe03cbfbaacee4c71b2
shiva-saini <shivasainisiyana2211@gmail.com>
2023-02-08 17:31:11 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,21 @@
 package com.driver;
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
 
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
+
+
 public class Main {
-  public static void main(){
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+  public static void main(String args[]){
       B obj = new B();
-      obj.meth();
+      System.out.println(obj.meth());
       B obj1 = new B();
       obj1.meth();
   }
